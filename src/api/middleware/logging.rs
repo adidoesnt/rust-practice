16426e3aef180eddc5864ec9ba3e04f280c00612
Banerjee Aditya <aditya.banerjee@activate.sg>
2024-10-17 15:33:17 +0800

    add coloured output to logging middleware

--- src/api/middleware/logging.rs
+++ src/api/middleware/logging.rs
@@ -5,6 +5,7 @@ use axum::{
     response::Response,
 };
 use chrono::{DateTime, Utc};
+use colour::green;
 use std::time::SystemTime;
 
 pub async fn logging_middleware(request: Request<Body>, next: Next) -> Response {
@@ -12,8 +13,9 @@ pub async fn logging_middleware(request: Request<Body>, next: Next) -> Response 
     let path: String = request.uri().path_and_query().unwrap().to_string();
     let start_time: SystemTime = SystemTime::now();
     let start_time: DateTime<Utc> = start_time.into();
-    
-    println!("[{}] {} {}", start_time, method, path);
+
+    green!("[{}] ", start_time);
+    println!("{} {}", method, path);
 
     let response: Response = next.run(request).await;
     response
